70ab2c2709b020217b38fd542c023288a6dd2c06
SantiagoNunezSelem <gigogisantisd@gmail.com>
2024-03-03 17:36:55 -0300

    Add User Menu working

--- src/componets/AddUser.js
+++ src/componets/AddUser.js
@@ -1,6 +1,54 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
-function AddUser({state,handleStateAddUserMenu}){
+function AddUser({state,handleStateAddUserMenu,searchUserName,addClient}){
+
+    const [formDataName,setFormDataName] = useState("")
+
+    const [formDataUserName,setFormDataUserName] = useState("")
+
+    const [errorMessage,setErrorMessage] = useState("")
+
+    const [stateErrorMessage,setStateErrorMessage] = useState("")
+
+    const handleSubmit = (ev) => {
+        const user = createUser()
+        ev.preventDefault();
+
+        //You can add a new user if their username is unique
+        if(!searchUserName(user)){
+            addClient(user)
+            clearInputs()
+            handleStateAddUserMenu() //Close the menu
+        }
+    }
+
+    const clearInputs = () => {
+        setFormDataName("")
+        setFormDataUserName("")
+    }
+
+    const createUser = () => {
+        const formData = {
+            name:formDataName,
+            username:formDataUserName
+        }
+        return formData
+    }
+
+    useEffect(() => {
+        const user = createUser()
+
+        if(searchUserName(user)){
+            setErrorMessage("ERROR: UserName already exist")
+            setStateErrorMessage(true)
+        }
+        else{
+            setErrorMessage("Valid UserName")
+            setStateErrorMessage(false)
+        }
+            
+    },[formDataUserName])
+
     return(
             <>
                 {state && //Only if the condition is true you can see the card
@@ -8,23 +56,46 @@ function AddUser({state,handleStateAddUserMenu}){
                         <div className="card position-absolute top-50 start-50 translate-middle">
                             
                             <div className="position-absolute d-flex justify-content-end mt-2" style={{width:"97%"}}> 
-                                <a onClick={handleStateAddUserMenu}><i class="bi bi-x-lg"></i></a> {/*Exit Icon*/}
+                                <a onClick={handleStateAddUserMenu}><i className="bi bi-x-lg"></i></a> {/*Exit Icon*/}
                             </div>
 
-                            <form class="row">
+                            <form onSubmit={handleSubmit} className="row">
                                 <div className="row d-flex g-3 align-items-center justify-content-center">
-                                    <div class="col-md-5">
-                                        <label for="inputName" class="form-label">Name</label>
-                                        <input type="text" class="form-control" id="inputName" required style={{ border: "1px solid black" }}/>
+                                    <div className="col-md-5">
+                                        <label for="inputName" className="form-label">Name</label>
+                                        <input 
+                                            onChange={ev => setFormDataName(ev.target.value)} 
+                                            type="text" className="form-control" 
+                                            id="inputName"
+                                            value={formDataName}
+                                            autoComplete="off"
+                                            required 
+                                            style={{ border: "1px solid black" }}
+                                        />
                                     </div>
-                                    <div class="col-md-5">
-                                        <label for="inputUserName" class="form-label">UserName</label>
-                                        <input type="text" class="form-control" id="inputUserName" required style={{ border: "1px solid black" }}/>
+                                    <div className="col-md-5">
+                                        <label for="inputUserName" className="form-label">UserName</label>
+                                        <input 
+                                            onChange={ev => setFormDataUserName(ev.target.value)} 
+                                            type="text" 
+                                            className="form-control" 
+                                            id="inputUserName"
+                                            value={formDataUserName}
+                                            autoComplete="off"
+                                            required 
+                                            style={{ border: "1px solid black" }}
+                                        />
+                                        {formDataUserName !== "" &&
+                                            <div className={`position-absolute ${stateErrorMessage ? "text-danger" : "text-success"}`}>
+                                                {errorMessage}
+                                            </div>
+
+                                        }
                                     </div>
                                 </div>
                                 
-                                <div class="col-12 mt-4 mb-4">
-                                    <button type="submit" class="btn btn-primary">SAVE</button>
+                                <div className="col-12 mt-4 mb-4">
+                                    <button type="submit" className="btn btn-primary">save</button>
                                 </div>
                             </form>
                         </div>
--- src/componets/Filter.js
+++ src/componets/Filter.js
@@ -19,25 +19,42 @@ function Filter(){
         setInputText(e.target.value)
     }
 
+    const searchUserName = (client) => {
+        const newFilterClientsJson = clientsJson.filter(clientFilter => clientFilter.username === client.username)
+        console.log(newFilterClientsJson)
+        if(newFilterClientsJson.length === 0) 
+            return false
+        else 
+            return true      
+    }
+
+    const addClient = (client) => {
+         //se pierde la info con el useEffect, tengo que guardaro en el clientes.json
+        clientsJson = [client,...clientsJson]
+        
+    }
+
     useEffect(() => {
         setFilterClientsJson(clientsJson.filter(client => 
             client.name.toLowerCase().includes(inputText.toLowerCase()) || 
             client.username.toLowerCase().includes(inputText.toLowerCase()))
             );
-    },[inputText])
+    },[inputText,clientsJson])
 
     return(
         <div className="w-100">
             <div className="container d-flex align-items-center justify-content-center mt-3">
                 <input className="w-25 " onChange={handleInputText}></input>
-                <button type="button" class="btn btn-secondary ms-2" onClick={handleStateAddUserMenu}>
-                    <i class="bi bi-person-plus"></i>
+                <button type="button" className="btn btn-secondary ms-2" onClick={handleStateAddUserMenu}>
+                    <i className="bi bi-person-plus"></i>
                 </button>
             </div>
 
             <AddUser 
                 state={stateAddUserMenu}
                 handleStateAddUserMenu={handleStateAddUserMenu}
+                searchUserName={searchUserName}
+                addClient={addClient}
             />
 
             <Table clients={filterClientsJson}/>
