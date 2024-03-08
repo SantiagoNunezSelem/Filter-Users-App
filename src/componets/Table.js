4fdf006be37c5df6daf354ab6244cef8c82627ff
SantiagoNunezSelem <gigogisantisd@gmail.com>
2024-03-08 15:08:04 -0300

    style of the table modified

--- src/componets/Table.js
+++ src/componets/Table.js
@@ -3,11 +3,12 @@ import "../App.css";
 
 function Table( {clients, deleteClient} ){
     return(
-        <table className='table w-75 m-auto mt-4 mb-5'>
+        <table className='table m-auto mt-4 mb-5'>
                 <thead>
                     <tr>
                         <th scope="col">NAME</th>
                         <th scope="col">USERNAME</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -16,9 +17,11 @@ function Table( {clients, deleteClient} ){
                         <tr key={index} >
                             <td>{client.name}</td>
                             <td>{client.username}</td>
-                            <a >
-                                <i onClick={() => deleteClient(client.username)} class="bi bi-trash3"></i> {/* Trash Icon */}
-                            </a> 
+                            <td id="trash-column">
+                                <a>
+                                    <i onClick={() => deleteClient(client.username)} class="bi bi-trash3"></i> {/* Trash Icon */}
+                                </a>
+                            </td>
                         </tr>)
                     }
                 </tbody>
